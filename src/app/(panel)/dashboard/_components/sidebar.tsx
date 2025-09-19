6e90e14a6f27a7fe2c322e74ba155e9c3205314f
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-09-19 07:36:24 -0300

    finalizado-menu-mobile

--- src/app/(panel)/dashboard/_components/sidebar.tsx
+++ src/app/(panel)/dashboard/_components/sidebar.tsx
@@ -3,12 +3,13 @@
 import clsx from "clsx"
 import { useState } from "react"
 import { usePathname } from "next/navigation"
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger} from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger} from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button"
-import { List } from "lucide-react"
+import { Banknote, CalendarCheck2, Folder, List, Settings } from "lucide-react"
+import Link from "next/link"
 export function SidebarDashboard( { children } : { children?: React.ReactNode } ) {
-    const usePathnames = usePathname()
-    const [isCollapsed, setIsCollapsed] = useState(true)
+    const pathame = usePathname()
+    const [isCollapsed, setIsCollapsed] = useState(false)
    
     return(
         <div className="flex min-h-screen w-full">
@@ -30,13 +31,44 @@ export function SidebarDashboard( { children } : { children?: React.ReactNode } 
                         </h1>
                     </div>
     
-                    <SheetContent side="right" className="sm:max-w-xs text-black">
+                    <SheetContent side="right" className="sm:max-w-xs text-black px-5">
                         <SheetTitle className="mt-3">PlanPro</SheetTitle>
                         <SheetDescription>
                             Menu PlanoPro
                         </SheetDescription>
-                        <nav>
-                            <p>Teste</p>
+                        <nav className="grid gap-2 text-base pt-5">
+                            <SidebarLink
+                                href="/dashboard"
+                                label="Agendamentos"
+                                pathname={pathame}
+                                isColapsed={isCollapsed}
+                                icon={<CalendarCheck2 className="w-6 h-6" />}
+                            />
+                            <SidebarLink
+                                href="/dashboard/services"
+                                label="Serviços"
+                                pathname={pathame}
+                                isColapsed={isCollapsed}
+                                icon={<Folder className="w-6 h-6"/>}
+                            />
+
+                            <hr className="bg-gray-700 border-2" />
+
+                            <SidebarLink
+                                href="/dashboard/profile"
+                                label="Meu Perfil"
+                                pathname={pathame}
+                                isColapsed={isCollapsed}
+                                icon={<Settings  className="w-6 h-6"/>}
+                            />
+
+                            <SidebarLink
+                                href="/dashboard/plans"
+                                label="Planos"
+                                pathname={pathame}
+                                isColapsed={isCollapsed}
+                                icon={<Banknote  className="w-6 h-6"/>}
+                            />
                         </nav>
                     </SheetContent>
                 </Sheet>
@@ -46,5 +78,27 @@ export function SidebarDashboard( { children } : { children?: React.ReactNode } 
             </main>
             </div>
         </div>
+    )
+}
+
+interface SidebarLinkProps {
+    href: string
+    icon: React.ReactNode
+    label: string
+    pathname: string
+    isColapsed: boolean
+}
+
+function SidebarLink( { href, icon, isColapsed, label, pathname } : SidebarLinkProps ) {
+    return(
+        <Link href={href}>
+            <div className={clsx("flex items-center gap-2 py-2 rounded-md transition-colors duration-200", {
+                "text-white font-semibold bg-blue-500" : pathname === href,
+                "text-gray-500 hover:bg-blue-300" : pathname !== href,
+            })}>
+                <span className="w-6 h-6 pl-3">{icon}</span>
+                { !isColapsed && <span className="pl-3">{label}</span> }
+            </div>
+        </Link>
     )
 }
