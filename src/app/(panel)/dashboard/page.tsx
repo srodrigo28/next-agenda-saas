584064ec5d071fe1e298b58454dbeae81731ac93
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-09-20 13:19:34 -0300

    user-types

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,7 +1,22 @@
-export default function Dashboard(){
+import getSession from '@/lib/getSession'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+
+export default async function Dashboard(){
+    const session = await getSession()
+
+    console.log(session?.user)
+
+    if(!session){
+        redirect("/")
+    }
+
     return(
         <div className="pr-11">
             <h1 className="text-3xl font-semibold"> Dashboard </h1>
+            <p>{session.user?.name} {session.user?.email}</p>
+            <p>{session.user.image}</p>
+
         </div>
     )
 }
