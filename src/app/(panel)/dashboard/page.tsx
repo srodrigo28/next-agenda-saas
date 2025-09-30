90eafda69c6f0d6e2fa22f12f921c3628f754965
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-09-29 22:19:55 -0300

    updated

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@/components/ui/button'
 import getSession from '@/lib/getSession'
+import { Calendar } from 'lucide-react'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
-
+import { ButtonCopyLink } from './_components/button-copy-link'
+ 
 export default async function Dashboard(){
     const session = await getSession()
 
@@ -11,11 +15,20 @@ export default async function Dashboard(){
     }
 
     return(
-        <div className="pr-11">
-            <h1 className="text-3xl font-semibold"> Dashboard </h1>
-            <p>{session.user?.name} {session.user?.email}</p>
-            <p>{session.user.image}</p>
+        <main>
+            <div className="space-x-2 flex items-center justify-end">
+                <Link href={`/clinica/${session.user?.id}`}>
+                    <Button className='bg-emerald-500 hover:bg-emerald-400 flex-1 md:flex-[0]'>
+                        <Calendar className='w-5 h-5' />
+                        <span>Novo agendamento</span>
+                    </Button>
+                </Link>
+                <ButtonCopyLink userId={session.user?.id!} />
+            </div>
 
-        </div>
+            <section className='grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4'>
+
+            </section>
+        </main>
     )
 }
