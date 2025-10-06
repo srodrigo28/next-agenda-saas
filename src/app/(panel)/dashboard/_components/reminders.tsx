3412d5359607c5dc04dfb20985dbb5d42b9dfa7b
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-10-05 22:35:42 -0300

    updated-remiders

--- src/app/(panel)/dashboard/_components/reminders.tsx
+++ src/app/(panel)/dashboard/_components/reminders.tsx
@@ -1,4 +1,10 @@
-export function Reminders(){
+import { getReminders } from '../_data-access/get-reminders'
+
+export async function Reminders( { userId } : { userId: string } ) {
+    const reminders = await getReminders({ userId: userId })
+
+    console.log("Lembretes encontrados: ", reminders)
+
     return(
         <div>
             <h1>Lembrate</h1>
--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { Calendar } from 'lucide-react'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 import { ButtonCopyLink } from './_components/button-copy-link'
+import { Reminders } from './_components/reminders'
  
 export default async function Dashboard(){
     const session = await getSession()
@@ -27,8 +28,8 @@ export default async function Dashboard(){
             </div>
 
             <section className='grid grid-cols-1 gap-4 lg:grid-cols-2 mt-4'>
-                
-
+                <div>AGENDA</div>
+                <Reminders userId={session.user?.id} />
             </section>
         </main>
     )
